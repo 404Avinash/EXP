579d81f67b31cbd7061b3c92c1f60d3d23f54d99
404Avinash <avinash1807007@gmail.com>
2025-11-29 02:19:46 +0530

    update

--- Testing/todo app/basic calc/script.js
+++ Testing/todo app/basic calc/script.js
@@ -49,7 +49,24 @@ function calculate() {
         display.value = "Error";
     }let display = document.getElementById("display");
 
+function press(vlet display = document.getElementById("display");
+
 function press(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "Error";
+    }
+}
+alue) {
     display.value += value;
 }
 
