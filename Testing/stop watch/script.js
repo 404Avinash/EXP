aea97d0f4aa24765072dee848169566a7642dd90
404Avinash <avinash1807007@gmail.com>
2025-11-29 03:21:13 +0530

    update

--- Testing/stop watch/script.js
+++ Testing/stop watch/script.js
@@ -4,3 +4,5 @@ let interval = null;
 function updateDisplay() {
     let hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
      let mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+
+     let secs = String(seconds % 60).padStart(2, '0');
