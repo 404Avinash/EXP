a1ff340504dcc5eb458aece24e842bb284009233
404Avinash <avinash1807007@gmail.com>
2025-11-29 02:20:59 +0530

    update

--- Testing/todo app/basic calc/script.js
+++ Testing/todo app/basic calc/script.js
@@ -7,15 +7,6 @@ function press(value) {
 function clearDisplay() {
     display.value = "";
 }
-let display = document.getElementById("display");
-
-function press(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = "";
-}
 
 function calculate() {
     try {
@@ -23,97 +14,4 @@ function calculate() {
     } catch (e) {
         display.value = "Error";
     }
-}
-
-function calculate() {
-    try {
-        displaylet display = document.getElementById("display");
-
-function press(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = "";
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
-}
-.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }let display = document.getElementById("display");
-
-function press(vlet display = document.getElementById("display");
-
-function press(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = "";
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
-}
-alue) {
-    display.value += value;
-}
-
-functlet display = document.getElementById("display");
-
-function press(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = "";
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
-}
-ion clearDisplay() {
-    display.value = "";
-}
-let display = document.getElementById("display");
-
-function press(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = "";
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
-}
-
 }
