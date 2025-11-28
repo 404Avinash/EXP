4fc5fc5a2e310ca4ace7fa7e21d0baca82fb7a8f
404Avinash <avinash1807007@gmail.com>
2025-11-29 01:38:38 +0530

    update

--- Testing/todo app/script.js
+++ Testing/todo app/script.js
@@ -3,82 +3,10 @@ function addTask() {
     let list = document.getElementById("taskList");
 
     if (input.value.trim() === "") return;
-function addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
 
     let item = document.createElement("li");
     item.textContent = input.value;
 
     list.appendChild(item);
     input.value = "";
-}
-
-    let item = document.createElement("li");
-    item.textContent = input.function addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
-
-    let item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appendChild(item);
-    input.value = "";
-}
-value;
-
-    list.appendChild(item);
-    input.valfunction addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
-
-    let item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appenfunction addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
-
-    let item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appendChild(item);
-    input.value = "function addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
-
-    let item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appendChild(item);
-    input.value = "";
-}
-";
-}
-dChild(item);
-    input.value = "function addTask() {
-    let input = document.getElementById("taskInput");
-    let list = document.getElementById("taskList");
-
-    if (input.value.trim() === "") return;
-
-    let item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appendChild(item);
-    input.value = "";
-}
-";
-}
-ue = "";
 }
