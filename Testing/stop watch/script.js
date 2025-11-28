614caa018fb726df729ec2897a594b3dd5fa1f1a
404Avinash <avinash1807007@gmail.com>
2025-11-29 03:24:49 +0530

    update

--- Testing/stop watch/script.js
+++ Testing/stop watch/script.js
@@ -3,31 +3,6 @@ let interval = null;
 
 function updateDisplay() {
     let hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
-     let mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
-
-     let secs = String(seconds % 60).padStart(2, '0');
-
-
-     document.getElementById("display").textContent = `${hrs}:${mins}:${secs}`;
-
-     }
-
-function startTimer() {
-
-    if (interval) return;
-
-    interval = setInterval(() => {
-
-
-
-          seconds++;
-        updateDisplay();
-    }, 1000);
-}let seconds = 0;
-let interval = null;
-
-function updateDisplay() {
-    let hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
     let mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
     let secs = String(seconds % 60).padStart(2, '0');
 
@@ -51,9 +26,4 @@ function resetTimer() {
     seconds = 0;
     updateDisplay();
     stopTimer();
-}
-
-function stopTimer() {
-    clearInterval(interval);
-    interval = null;
 }
