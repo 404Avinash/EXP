0099a5c095be09eb70cda73922a1b99db7e3e520
404Avinash <avinash1807007@gmail.com>
2025-11-29 01:35:20 +0530

    update

--- Testing/todo app/script.js
+++ Testing/todo app/script.js
@@ -3,6 +3,18 @@ function addTask() {
     let list = document.getElementById("taskList");
 
     if (input.value.trim() === "") return;
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let list = document.getElementById("taskList");
+
+    if (input.value.trim() === "") return;
+
+    let item = document.createElement("li");
+    item.textContent = input.value;
+
+    list.appendChild(item);
+    input.value = "";
+}
 
     let item = document.createElement("li");
     item.textContent = input.value;
